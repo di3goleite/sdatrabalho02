0b3d2552739e90ba8c92411fa5c497c50e3e2ff9
Diego Leite <diegojleite@gmail.com>
2014-07-24 05:51:36 -0300

    O relógio de lamport funciona

--- lamport.py
+++ lamport.py
@@ -27,22 +27,30 @@ while(count < eventos):
     p_recebe = int(raw_input("=> "))
 
     if p_envia == p_recebe:
-        print "m = %s" % relogio[p_envia][-1]
         relogio[p_envia].append(relogio[p_envia][-1] + 1)
+        relogio[p_recebe].append(relogio[p_recebe][-1] + 1)
+        print "\n# TIPO: Evento Interno"
+        print "# M = %s" % relogio[p_envia][-2]
+        print "# R de P%s = %s\n" % (p_envia, relogio[p_envia][-1])
     else:
+        relogio[p_envia].append(relogio[p_envia][-1] + 1)
+        print "\n# TIPO: Evento Entre Processos"
+        print "# M = %s" % relogio[p_envia][-1]
+        print "# R de P%s = %s" % (p_envia, relogio[p_envia][-1])
+
         if relogio[p_envia][-1] > relogio[p_recebe][-1]:
-            print "m = %s" % relogio[p_envia][-1]
             relogio[p_recebe].append(relogio[p_envia][-1] + 1)
+            print "# R de P%s = %s" % (p_recebe, relogio[p_recebe][-1])
         else:
-            print "m = %s" % relogio[p_envia][-1]
             relogio[p_recebe].append(relogio[p_recebe][-1] + 1)
+            print "# R de P%s = %s" % (p_recebe, relogio[p_recebe][-1])
 
     log.append("[%s]---->" % p_envia)
     log.append("[%s]" % p_recebe)
     count = count + 1
 
 print "\n"
-
+print "Histórico: ",
 for l in log:
     print l,
 print "\n"
@@ -50,7 +58,7 @@ print "\n"
 i=0
 
 for r in relogio:
-    print "Clock %s:" % i,
+    print "Relógio de P%s:" % i,
     print r
     i = i + 1
 print "\n"
